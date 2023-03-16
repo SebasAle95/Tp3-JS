c6aa1774f3c8489c0e0d00da5442eaf45d9e3019
Sebastian Orosco <sebas_ale05@hotmail.com>
2023-03-15 23:27:12 -0300

    ejercicio 4

---
+++ js/4-ejercicioF.js
@@ -0,0 +1,10 @@
+function informacionCadena(cadena) {
+    if (cadena === cadena.toUpperCase()) {
+      document.write("La cadena está formada solo por mayúsculas.");
+    } else if (cadena === cadena.toLowerCase()) {
+      document.write("La cadena está formada solo por minúsculas.");
+    } else {
+      document.write("La cadena está formada por una mezcla de mayúsculas y minúsculas.");
+    }
+  }
+informacionCadena("HOLA"); // La cadena está formada solo por mayúsculas.
